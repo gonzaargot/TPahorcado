e05cc3ca8516305b7e368c1a3acfb8bb82138c32
Matias Bangueses <matiasluisb13@gmail.com>
2021-02-04 17:30:39 -0300

    ordené los archivos

--- Ahorcado.py
+++ Ahorcado.py
@@ -1,4 +1,8 @@
-import modulos.generador_archivos_ordenados as listar_cuentos
+from modulos import generador_archivos_ordenados as listar_cuentos
+from modulos import configuracion
 
 def main():
     listar_cuentos.generar_archivos()
+    max_usuarios, long_palabra_min,max_desaciertos, puntos_aciertos, puntos_desaciertos, puntos_adivina = configuracion.configuracion()
+    
+main()
